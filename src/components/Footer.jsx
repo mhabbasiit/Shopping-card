16194043458c1ed4cd9d5c31483923e70bd12920
mhabbasi.it <mhabbasi.it@gmail.com>
2022-09-03 09:30:44 -0700

    "chnage mhabbasi"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -122,7 +122,7 @@ function Footer() {
           <div className="text-center text-dark p-3 text-bg-primary">
             © 2020 Copyright:
             <a className="text-dark footer" href="/#">
-              Mohammadhassan Abbasi
+              Mohammadhassan Abbasi 
             </a>
           </div>
         </footer>
